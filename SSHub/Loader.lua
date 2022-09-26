4f557b017497b1289e119e60f3be1190de15fbe0
Tupi <65321514+miguel831@users.noreply.github.com>
2022-09-25 20:05:37 -0400

    Update Loader.lua

--- SSHub/Loader.lua
+++ SSHub/Loader.lua
@@ -21,19 +21,31 @@ local Test = coroutine.wrap(function()
 	print("Testing Loader")
 end)
 local Criminality = coroutine.wrap(function()
+	if not game:GetService("CoreGui"):FindFirstChild("SSHub") then
 	print("[2/2] Game found: "..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name)
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/miguel831/Roblox-Scripts/main/SSHub/Scripts/SSHub_Criminality.lua'))()
+	elseif game:GetService("CoreGui"):FindFirstChild("SSHub") then
+		game:GetService("Players").LocalPlayer:Kick("\nScript Alredy Loaded")
+	end
 end)
+
+local BloxFruits = coroutine.wrap(function()
+	if not game:GetService("CoreGui"):FindFirstChild("SSHub") then
+	print("[2/2] Game found: "..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name)
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/miguel831/Roblox-Scripts/main/SSHub/Scripts/SSHub_BloxFruit.lua'))()
+	elseif game:GetService("CoreGui"):FindFirstChild("SSHub") then
+		game:GetService("Players").LocalPlayer:Kick("\nScript Alredy Loaded")
+	end
+end)
+
 --#endregion
 local SupportedGames = loadstring(game:HttpGet('https://raw.githubusercontent.com/miguel831/Roblox-Scripts/main/SSHub/Games.lua', true))()
 print("Stage [1/2] Game Checker")
 print("[1/2] Checking...")
-if SupportedGames[game.PlaceId] then
-	if not game:GetService("CoreGui"):FindFirstChild("SSHub") then
-		Load(Criminality)
-	elseif game:GetService("CoreGui"):FindFirstChild("SSHub") then
-		game:GetService("Players").LocalPlayer:Kick("\nScript Alredy Loaded")
-	end
+if SupportedGames == 8343259840 then
+	Load(Criminality)
+elseif SupportedGames == 2753915549 or 4442272183 or 7449423635 then
+	Load(BloxFruits)
 else
 	print("[2/2] No game found!")
 end
