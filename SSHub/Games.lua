b85e8e6b9e226ee00d27c420c7d4c00441e236b0
Tupi <65321514+miguel831@users.noreply.github.com>
2022-09-25 23:12:22 -0400

    Update Games.lua

--- SSHub/Games.lua
+++ SSHub/Games.lua
@@ -1,8 +1,21 @@
 GameSupport = {
-  [8343259840] = true,
-  [2753915549] = true,
-  [4442272183] = true,
-  [7449423635] = true,
-}
-
+	["8343259840"] = {
+		Name = "Criminality",
+		Script = "Criminality"
+	},
+	["2753915549"] = {
+		Name = "BloxFruits1",
+		Script = "BloxFruit"
+	},
+	["4442272183"] = {
+		Name = "BloxFruits2",
+		Script = "BloxFruit"
+	},
+	["7449423635"] = {
+		Name = "BloxFruits3",
+		Script = "BloxFruit"
+	},
+  }
+  
 return GameSupport
+  
