6acd35903e08bea5f77b5b41f4e8a300a8002cbf
Tupi <65321514+miguel831@users.noreply.github.com>
2022-11-28 18:49:56 -0400

    Update ProjectSlayers.lua

--- SSHub/Scripts/ProjectSlayers.lua
+++ SSHub/Scripts/ProjectSlayers.lua
@@ -288,6 +288,13 @@ Plr.CharacterAdded:Connect(function()
     Settings.PlrDied = false
 end)
 
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+    wait(1)
+    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+
 coroutine.wrap(function()
     while task.wait() do
         pcall(function()
