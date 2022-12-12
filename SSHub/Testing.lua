e282606f24987665ce59c0d9a06dd3a1b2d2075a
Tupi <65321514+miguel831@users.noreply.github.com>
2022-12-12 19:43:42 -0400

    Update Testing.lua

--- SSHub/Testing.lua
+++ SSHub/Testing.lua
@@ -1,68 +1,294 @@
-local BPOwner = true
-local WeebHook = "https://discord.com/api/webhooks/1046955945621737502/HsniRn_0kRUhfXqIb5eI6csZqYwQwWOFm-d1ZJKwk4eoFvvELEnxsD8u_ow-koBGqNzB"
-local ExecutorUsing = is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or secure_load and "Sentinel" or KRNL_LOADED and "Krnl" or SONA_LOADED and "Sona" or "WTF?"
-local HttpService = game:GetService("HttpService")
-local UserId = game.Players.LocalPlayer.UserId
-local SupportedGames = loadstring(game:HttpGet('https://raw.githubusercontent.com/miguel831/Roblox-Scripts/main/SSHub/Games.lua', true))()
-local function GetSupportedGame() 
-	local Game
-    for Id, Info in pairs(SupportedGames) do
-        if tostring(game.PlaceId) == Id then
-            Game = Info break
+repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
+
+if game.Players.LocalPlayer.Team == nil then
+    task.wait(2)
+    local Button = game.Players.LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.ViewportFrame.TextButton
+    local Events = {"MouseButton1Click", "MouseButton1Down", "Activated"}
+    for i,v in pairs(Events) do
+        for i,v in pairs(getconnections(Button[v])) do
+            v.Function()
         end
-    end return Game
+    end
 end
-local SupportedGame = GetSupportedGame()
-local Info = function()
-    if BPOwner and UserId == (860289947 or 446350986) then
-        print("Bypassed")
-    else
-        local Data = {
-            ["username"] = "SSHub",
-            ["embeds"] = {
-                {
-                    ["title"] = "Script Executed",
-                    ["color"] = 9893552,
-                    ["fields"] = {
-                        {
-                            ["name"] = "User",
-                            ["value"] = game.Players.LocalPlayer.Name,
-                            ["inline"] = true
-                        },
-                        {
-                            ["name"] = "Id",
-                            ["value"] = game.Players.LocalPlayer.UserId,
-                            ["inline"] = true
-                        },
-                        {
-                            ["name"] = "Age",
-                            ["value"] = game.Players.LocalPlayer.AccountAge,
-                            ["inline"] = true
-                        },
-                        {
-                            ["name"] = "Executor",
-                            ["value"] = ExecutorUsing,
-                            ["inline"] = true
-                        },
-						{
-                            ["name"] = "Game",
-                            ["value"] = SupportedGame.Name,
-                            ["inline"] = true
-                        },
-						{
-                            ["name"] = "Script",
-                            ["value"] = SupportedGame.Script,
-                            ["inline"] = true
-                        }
+local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
+local Module = loadstring(game:HttpGet"https://raw.githubusercontent.com/miguel831/Roblox-Scripts/main/SSHub/ServerHop")()
+local NS = {
+	Title = "SSHub",
+	Icon = "rbxassetid://8426126371"
+}
+local function Notify(Title, Icon, Text, Duration)
+	game.StarterGui:SetCore("SendNotification", {Title = Title or ""; Text = Text or ""; Icon = Icon or ""; Duration = tonumber(Duration) or 3 })
+end
+-- #region Services
+local game = game;
+local GetService = game.GetService;
+local Workspace = GetService(game, "Workspace")
+local Players = GetService(game, "Players")
+local ReplicatedStorage = GetService(game, "ReplicatedStorage")
+local ScriptContext = GetService(game, "ScriptContext")
+local LogService = GetService(game, "LogService")
+local RunService = GetService(game, "RunService")
+local Lighting = GetService(game, "Lighting")
+local CoreGui = GetService(game, "CoreGui");
+local Uis = GetService(game, "UserInputService");
+local Cam = Workspace.CurrentCamera;
+local Mouse = Players.LocalPlayer:GetMouse()
+local Plr = Players.LocalPlayer
+local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+
+local Inv = ReplicatedStorage.Remotes.CommF_:InvokeServer("getInventoryFruits")
+local Backpack = Plr.Backpack
+--#endregion
+local function TableRemove(Table, Item)
+    local Index = nil
+    for i, v in ipairs (Table) do 
+        if (v == Item) then
+            Index = i 
+        end
+    end
+table.remove(Table, Index)
+end
+
+local ListFruits = {
+    "Bomb",
+    "Spike",
+    "Chop",
+    "Spring",
+    "Kilo",
+    "Smoke",
+    "Spin",
+    "Flame",
+    "Falcon",
+    "Ice",
+    "Sand",
+    "Dark",
+    "Diamond",
+    "Light",
+    "Love",
+    "Rubber",
+    "Barrier",
+    "Magma",
+    "Door",
+    "Quake",
+    "Human-Human: Buddha",
+    "String",
+    "Bird-Bird: Phoenix",
+    "Rumble",
+    "Paw",
+    "Gravity",
+    "Dough",
+    "Venom",
+    "Control",
+    "Dragon",
+    "Leopard"
+}
+local FruitsInBackPack = {}
+
+local Noclip = false
+    coroutine.wrap(function()
+        RunService.Stepped:Connect(function()
+            if Noclip then
+                for i, v in pairs(Plr.Character:GetDescendants()) do
+                    if v:IsA("BasePart") and v.CanCollide == true then
+                        v.CanCollide = false
+                    end
+                end
+                if not Noclip then
+                    for i, v in pairs(Plr.Character:GetDescendants()) do
+                        if v:IsA("BasePart") and v.CanCollide == false then
+                            v.CanCollide = true
+                        end
+                    end
+                end
+            end
+        end)
+    end)()
+
+    local Tween
+    local function TeleportTween(To)
+        if Tween then Tween:Cancel() Tween = nil end
+        local Speed
+        local Distance = (To.Position - Plr.Character.PrimaryPart.Position).Magnitude
+        if Distance < 500 then
+            Plr.Character.PrimaryPart.CFrame = To
+            Speed = 350
+        elseif Distance < 1200 then
+            Speed = 350
+        elseif Distance >= 1200 then
+            Speed = 300
+        end
+        local Plataform
+        if not workspace:FindFirstChild("TweeningPlataform") then
+            Plataform = Instance.new("Part", workspace)
+            Plataform.Transparency = 1
+            Plataform.Name = "TweeningPlataform"
+            Plataform.Size = Vector3.new(30,1,30)
+            Plataform.Anchored = true
+            Plataform.CFrame = Plr.Character.PrimaryPart.CFrame*CFrame.new(0,0,0)
+        end
+        Tween = game:GetService("TweenService"):Create(Plataform,TweenInfo.new(Distance/Speed, Enum.EasingStyle.Linear),{CFrame = To})
+        local NoFall
+        if not Plr.Character.PrimaryPart:FindFirstChild("BodyVelocity") then
+            NoFall = Instance.new("BodyVelocity", Plr.Character.PrimaryPart)
+            NoFall.Velocity = Vector3.new(0,0,0)
+        end
+        Noclip = true
+        Tween:Play()
+        coroutine.wrap(function()
+            repeat task.wait()
+                Plr.Character.PrimaryPart.CFrame = Plataform.CFrame
+            until not Tween or not Plataform or Distance < 10
+        end)()
+        Tween.Completed:Connect(function()
+            pcall(function()
+                Plataform:Destroy()
+                NoFall:Destroy()
+                Tween = nil
+                Noclip = false
+            end)
+        end)
+    end
+
+for _, Object in next, Backpack:GetChildren() do
+	for i,v in pairs(ListFruits) do
+		if string.find(Object.Name, v) and not string.find(Object.Name, "Talon") then
+		    table.insert(FruitsInBackPack, tostring(Object))
+		end
+	end
+end 
+Backpack.ChildAdded:Connect(function(Object)
+	for i,v in pairs(ListFruits) do
+		if string.find(Object.Name, v) and not string.find(Object.Name, "Talon") then
+		    table.insert(FruitsInBackPack, tostring(Object))
+		end
+	end
+end)
+Backpack.ChildRemoved:Connect(function(Object)
+	for i,v in pairs(ListFruits) do
+		if string.find(Object.Name, v) and not string.find(Object.Name, "Talon") then
+            TableRemove(FruitsInBackPack, tostring(Object))
+        end
+    end
+end)
+local Error
+local function PickFruits()
+    for _,v in pairs(Workspace:GetChildren()) do
+        if string.find(tostring(v), "Fruit" or "Fruit ") and v.Parent == Workspace then
+            local Time1 = 0
+            task.spawn(function()
+                repeat task.wait(1)
+                    Time1 = Time1 + 1
+                until Time1 == 3 or not v.Parent == Workspace
+            end)
+            while task.wait() and string.find(tostring(v), "Fruit" or "Fruit ") and v.Parent == Workspace and Time1 < 3 do
+				Plr.Character.HumanoidRootPart.CFrame = v:FindFirstChild("Handle").CFrame
+				firetouchinterest(Plr.Character.HumanoidRootPart, v:FindFirstChild("Handle"), 0)
+				firetouchinterest(Plr.Character.HumanoidRootPart, v:FindFirstChild("Handle"), 1)
+				Plr.Character.HumanoidRootPart.CFrame = v:FindFirstChild("Handle").CFrame*CFrame.new(0,0,.5)
+            end
+            wait(2)
+            if Time1 >= 3 and v.Parent == Workspace then
+                Notify(NS.Title,NS.Icon,"Tweening")
+                task.wait(5)
+                TeleportTween(v:FindFirstChild("Handle").CFrame)
+                local Time = 0
+                task.spawn(function()
+                    repeat task.wait(1)
+                        Time = Time + 1
+                    until Time == 3 or not v.Parent == Workspace
+                end)
+                while task.wait() and string.find(tostring(v), "Fruit" or "Fruit ") and v.Parent == Workspace and Time < 3 do
+                    Plr.Character.HumanoidRootPart.CFrame = v:FindFirstChild("Handle").CFrame
+                    firetouchinterest(Plr.Character.HumanoidRootPart, v:FindFirstChild("Handle"), 0)
+                    firetouchinterest(Plr.Character.HumanoidRootPart, v:FindFirstChild("Handle"), 1)
+                    Plr.Character.HumanoidRootPart.CFrame = v:FindFirstChild("Handle").CFrame*CFrame.new(0,0,.5)
+                end
+                if Time >= 3 then
+                    task.wait(3)
+                    Notify(NS.Title,NS.Icon,"Can't get the fruit.")
+                end
+            end
+		end
+    end
+end
+
+local function CheckInventory()
+    local InvFruits = {}
+    for _,Table in pairs(Inv) do
+        for _,v in pairs(Table) do
+            for _,i in pairs(ListFruits) do
+                if string.find(v, i) then
+                    table.insert(InvFruits, i)
+                end
+            end
+        end
+    end
+    return InvFruits
+end
+
+local function CheckBackpack()
+    local TempTable = {}
+    local fruit
+    for _,i in pairs(CheckInventory()) do
+        for _,v in pairs(FruitsInBackPack) do
+            if not string.match(v, i) then
+                fruit = v.." Fruit"
+                table.insert(TempTable, v:sub(0,#v - #" Fruit"))
+            end
+        end
+        break
+    end
+    return TempTable, fruit
+end
+
+local function StoreFruits()
+    Plr.Character.Humanoid:UnequipTools()
+    pcall(function()
+		for _,v in pairs(CheckBackpack()) do
+            if Backpack:FindFirstChild(v.." Fruit") then
+			    ReplicatedStorage.Remotes.CommF_:InvokeServer("StoreFruit", v.."-"..v, Backpack[v.." Fruit"])
+			end
+		end
+	end)
+end
+
+PickFruits()
+if getgenv().WeebHook then
+    local a, Fruit = CheckBackpack()
+    local HttpService = game:GetService("HttpService")
+    local Data = {
+        ["username"] = "SSHub",
+        ["embeds"] = {
+            {
+                ["title"] = "Fruit Picked!",
+                ["color"] = 9893552,
+                ["fields"] = {
+                    {
+                        ["name"] = "Fruit",
+                        ["value"] = Fruit
                     }
                 }
             }
         }
-        local Headers = {["Content-Type"] = "application/json"}
-        local Encoded = HttpService:JSONEncode(Data)
-        local Request = http_request or request or HttpPost or syn.request
-        local Final = {Url = WeebHook, Body = Encoded, Method = "POST", Headers = Headers}
-        Request(Final)
-    end
+    }
+    local Headers = {["Content-Type"] = "application/json"}
+    local Encoded = HttpService:JSONEncode(Data)
+    local Request = http_request or request or HttpPost or syn.request
+    local Final = {Url = getgenv().WeebHook, Body = Encoded, Method = "POST", Headers = Headers}
+    Request(Final)
 end
-return Info
+StoreFruits()
+
+if getgenv().HoopServers then
+    Notify(NS.Title,NS.Icon,"U have 3 seconds if u want desactive hoopserver! Re-Execute the script with getgenv().HoopServers = false")
+    task.wait(3)
+end
+if getgenv().HoopServers then
+    Notify(NS.Title,NS.Icon,"Auto hooping servers...")
+    task.wait(2)
+    queueteleport("getgenv().HoopServers = true")
+    queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/miguel831/Roblox-Scripts/main/SSHub/Scripts/AutoPickFrutis.lua'))()")
+    Module:Teleport(game.PlaceId)
+else
+    Notify(NS.Title,NS.Icon,"Auto hoop servers off!\nActivate it on settings.")
+end
